0f672ad229f4bb4e21caf657926e0aa9cdbfa2a2
hayabusa88magic <30853550+hayabusa88magic@users.noreply.github.com>
2017-10-26 13:36:03 +0900

    最新

--- app/src/main/java/com/example/ble_test/MainActivity.java
+++ app/src/main/java/com/example/ble_test/MainActivity.java
@@ -7,42 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.*;
-//＊は関係のあるものすべて
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-//  http://dev.classmethod.jp/etc/22853/　→ハンドラとルーパー処理に関する記事
-//  別スレッドから、画面更新処理をするときはメッセージキューのしくみをつかう
+
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import java.util.UUID;
 
-//インプリメントとはインターフェイスの実装クラスを宣言するための予約語
-//「class クラス名 implements インターフェイス名
-//	interface→実装のないクラス。フィールドを持たず、実装のないメソッドを持つ。interfaceという予約語で宣言する。
-//  メソッドが実装されていないため、インターフェイス単体では何の機能も持たない。implementsという予約語を使用して
-// 「継承」のように実装クラスを作り、インターフェイスのメソッドを「オーバーライド」するように実装する。
-//「実装」メソッドの「本体」。メソッドの宣言直後にある{}で囲まれた箇所を「メソッドの実装」と呼ぶ。
-//メソッド→　クラス内に「戻り値の型 メソッド名( 引数の型 引数の変数名 ){ 実装 }」という書式でメソッドを作ることができる。
 public class MainActivity extends Activity implements BluetoothAdapter.LeScanCallback {
-//    BluetoothAdapterクラスのLeScanCallbackインターフェイスを実装
-//    インタフェースは1つのクラスに対し、複数実装することもできます
-//    クラスがextendsを使用する場合は、implementsはextendsの後に指定します。
-//    http://www.javaroad.jp/java_interface3.htm
-//    インタフェースを実装したクラスでは、インタフェースで宣言されたメンバ変数を「単純名」で参照できます。インタフェースを実装していないクラスからは「インタフェース名．単純名」で参照する必要があります。
-//    インタフェースで定義されたメソッドを参照する場合、必ずそのインタフェースを実装したクラスのオブジェクトを通して参照する必要があります。
-//    Java インタフェースとは何か 内容に抽象メソッドしか持たないクラスのようなものをインタフェースと呼びます。 クラスと並んで、パッケージのメンバーとして存在します。
-//    インタフェースはクラスによって実装 (implements) され、実装クラスはインタフェースで宣言されている抽象メソッドを実装します
-//     https://www.gixo.jp/blog/5159/
-        /** BLE 機器スキャンタイムアウト (ミリ秒) */
+
     private static final long SCAN_PERIOD = 10000;
-//   long = (-9223372036854775808L ～ 9223372036854775807L)が表現範囲
-//  唯一の使用機会はpublic static finalフィールドとして「定数値」フィールドを宣言する方法である。
-//  この場合、フィールドの値は変更されないため、複数のインスタンスからアクセスされても問題ない。
-//  staticフィールドはこのためにあると考えてもいいだろう。
-    /** 検索機器の機器名 */
+
     private static final String DEVICE_NAME = "Sensor";
     /** 対象のサービスUUID */
     private static final String DEVICE_BUTTON_SENSOR_SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb";
@@ -54,37 +33,23 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
     private static final String TAG = "BLE_Test";
 //    private final static String TAG = クラス.class.getSimpleName();
     private BleStatus mStatus = BleStatus.DISCONNECTED;
-//    非パブリックなインスタンスフィールドは頭文字を m とする→変数＝フィールド
-//    スタティックフィールドは頭文字を s とする。
+
     private Handler mHandler;
-//    Handlerインスタンスを生成したスレッドへイベントを送るための仕組みなのである。
-//    別スレッドでキューを管理するためのクラス
+
     private BluetoothAdapter mBluetoothAdapter;
-//    下記二つのクラスをインポートしたからそれに対して宣言をする
-//   import android.bluetooth.BluetoothAdapter;
-//   import android.bluetooth.BluetoothManager;
-//  クラスをインポートしてインスタンス化するときの名前の付け方のパターン→「mクラス名」が一般的なのかもしれない
+
     private BluetoothManager mBluetoothManager;
     private BluetoothGatt mBluetoothGatt;
     private TextView mStatusText;
 
     @Override
-// サブクラスでメソッドの上書きをする機能。
-// サブクラスでスーパークラスと同じメソッド名、引数、戻り値のメソッドを定義することで、スーパークラスのメソッドではなく、サブクラスのメソッドが呼ばれるようにする機能
     public void onCreate(Bundle savedInstanceState) {
-//              onCreateメソッドの引数Bundle savedInstanceStateの意味はactivityが再構築されるときに入れる決まりのもの
         super.onCreate(savedInstanceState);
-//      スーパークラスにあるonCreateメソッドを最初に呼び出して、スーパークラス側の処理を全部済ませてから、自分の処理を行うようにしているのです。それが、super.onCreate……という部分
         setContentView(R.layout.activity_main);
-//      setContentView()メソッドは、文字列などのViewを画面(Activity)に追加します
 
         mBluetoothManager = (BluetoothManager)getSystemService(BLUETOOTH_SERVICE);
-//      getSystemService(BLUETOOTH_SERVICE);→Object getSystemService (String name)_システムレベルのサービスのハンドルを取得するメソッドです。
-//      object→すべてのクラスはスーパークラスのオブジェクト
-//      BluetoothManagerはここではObjectなのか？
-        mBluetoothAdapter = mBluetoothManager.getAdapter();
-//      mBluetoothManagerについてのみ更新をかける意味のようだ
 
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
         findViewById(R.id.btn_connect).setOnClickListener(new View.OnClickListener() {
 //            https://qiita.com/HideMatsu/items/2e6caec8265bcf2a2dcb　→ボタン処理の書き方
 //            button.setOnClickListener(new OnClickListener() {
@@ -95,10 +60,7 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
 //            id="@+id/名前"でレイアウトファイルの中でIDが設定できる
             @Override
             public void onClick(View v) {
-//                onClickメソッドはView型の引数をとり、その変数名をvとするということです。
-//                戻り型　メソッド名　（引数型　引数名）　｛メソッド本体 ｝；
                 connect();
-//                メソッドconnectの呼び出し
             }
         });
         findViewById(R.id.btn_disconnect).setOnClickListener(new View.OnClickListener() {
@@ -108,9 +70,7 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
                 disconnect();
             }
         });
-
         mStatusText = (TextView)findViewById(R.id.text_status);
-//        キャスト→先頭の（）はキャスト
 
         mHandler = new Handler() {
             @Override
